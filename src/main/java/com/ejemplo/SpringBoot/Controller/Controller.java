553b30bdda8c442c714934e23065f2a108b9965b
Valeria Becker <valebecker@gmail.com>
2023-05-06 22:14:58 -0300

    Cambio de link en politica de CORS @CrossOrigin del controlador

--- src/main/java/com/ejemplo/SpringBoot/Controller/Controller.java
+++ src/main/java/com/ejemplo/SpringBoot/Controller/Controller.java
@@ -15,7 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://porfoliovaleria-1b776.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class Controller {
     
